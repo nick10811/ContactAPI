68d3e54b0426e2529e129009c785fabfa6cbb73c
Nick Yang <nick10811@gmail.com>
2021-02-05 16:48:44 +0800

    first commit

---
+++ ContactAPI/Model.swift
@@ -0,0 +1,15 @@
+//
+//  Model.swift
+//  ContactAPI
+//
+//  Created by Nick Yang on 2021/2/5.
+//
+
+import Foundation
+
+struct ContactModel {
+    var firstName: String
+    var lastName: String
+    var email: String?
+    var mobile: String?
+}
